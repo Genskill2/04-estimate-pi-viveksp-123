feb91950ec15ab70da98834b0b4f1445c22161da
VIVEK SP <spvivek2000@gmail.com>
2021-06-04 12:31:01 +0530

    first commit

--- wallis.c
+++ wallis.c
@@ -5,13 +5,28 @@
 
 float wallis_pi(int);
 
+float wallis_pi(int n)
+{
+float k=1.0;
+   int i;
+    for(i=1;i<=n;i++) 
+     {
+        k *=(float)(4.*i*i)/(4.*i*i-1);
+       }
+
+
+  return (k*2);
+}
+    
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
     pi = wallis_pi(i);
+    //printf("wallis called %f %f\n",M_PI,pi) ;
     if (!(fabs(pi - M_PI) > 0.15)) {
-      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
-      abort();
+      //printf("wallis g %f\n",pi) ;
+  printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
+          abort();
     }
   }
 
@@ -22,5 +37,7 @@ int main(void) {
       abort();
     }
   }
+  
 }
+
 
