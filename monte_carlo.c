65d4f2c24878119862017c5440f9493b6b47d847
viveksp-123 <85231919+viveksp-123@users.noreply.github.com>
2021-06-04 13:52:55 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -2,40 +2,56 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+float mc_pi(int);
 
-float Wallis(int n)
-{
-
-    if n <= 0
-        return 0;
-
-    float pi = 1;
-
-    for (i=0;i<n+1;i++)
-    {
-
-        pi *= (4 * (i*i))/(4(i*i) - 1);
-    }
-    return pi*2;
-    
+float frandom() {
+  long int q = random();
+  float ret = (float)q/(float)RAND_MAX;
+  return ret;
 }
-float MonteCarlo(int n):
 
-    if n <= 0
-        return 0;
-
-    int count = 0;
-
-    for (i=0;i<n;i++)
-    {
-
-        x = frandom();
-        y = frandom();
-
-        if x*x + y*y < 1
-            count += 1;
+int main(void) {
+  float pi0;
+  float pi1;
+  
+  pi0 = mc_pi(25000);
+  pi1 = mc_pi(25000);
+  printf("%f %f\n", pi0, pi1);
+  
+  if (pi0 == pi1) {
+      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
+      abort();
     }
-    return 4*count/n;
+
+  if (fabs(pi0 - pi1) > 0.05) {
+      printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
+      abort();
+  }
+
+    
+  for (int i=2000; i<5000; i++) {
+    pi0 = mc_pi(i);
+    if (!(fabs(pi0 - M_PI) < 0.4)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
+      abort();
+    }
+  }
+}
+float mc_pi(int count)
+{
+	float circle=0;
+	for(int i=0;i<count;i++)
+	{
+		
+		float x = frandom();
+		float y = frandom();
+		if(sqrt(x*x+y*y)<=1)
+		{
+			circle++;
+		}
+	}
+	float area =(float) (4*circle)/(count);
+	return area;
 }
 
 
